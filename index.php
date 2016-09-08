7d3d32cb7c4e46d7263f89be912a36464ced4cd5
starwarsgood <comp822012016@mail.ru>
2016-09-08 17:53:17 +0300

    kgkg

--- index.php
+++ index.php
@@ -1,9 +1,56 @@
-﻿<meta charset="UTF-8">
-<?
-$git=6;
-echo $git;
-if (isset($git))
-	echo "Переменная сущ.";
-else 
-	echo "Переменная не сущ.";
+﻿<meta charset="Utf-8">
+<?php
+$example=33;
+    echo $example;
+    echo "</br>";
+ if (is_integer($example))
+    echo "yes";
+    echo "</br>";
+ if (isset($example))
+    echo "yes";
+    echo "</br>";
+ if (is_double($example))
+    echo "yes";
+    echo "</br>";
+$taptap="тест";
+    echo $taptap;
+    echo "</br>";
+ if (isset($taptap)) {
+    echo "yes";
+    }
+    else  {
+       echo "</br>";
+       echo "no";
+       }
+       echo "</br>";
+$MyArray=array(
+         1 => "a",
+         2 => "b",
+         3 => "c",
+         4 => "d",
+	);
+echo "<pre>";
+print_r($MyArray);
+       echo "</br>";
+var_dump($MyArray);
+       echo "</br>";
+var_export($MyArray);
+       echo "</br>";
+       echo "</pre>";
+
+define("PI","3,14",true);
+echo PI;
+echo "</br>";
+ 
+$sdf="Пример";
+unset ($sdf);
+echo sdf;
+echo "</br>";
+
+$sdfs="Пример";
+$random=43;
+$result=$sdfs.$random;
+echo $result;
+echo "</br>";
+
 ?> 
